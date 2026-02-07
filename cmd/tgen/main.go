4aa280ca228350d9c9b9ce51037a1cf286a3d172
Andrey Chernykh <andreychernykh04@gmail.com>
2026-02-07 09:52:13 +0300

    #1: add initial workflow configurations for CI tools and linting

---
+++ cmd/tgen/main.go
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2026 Andrey Chernykh
+// SPDX-License-Identifier: MIT
+
+// This is the main entry point for the tgen tool.
+package main
+
+import "fmt"
+
+func main() {
+	//nolint:forbidigo // simple output to indicate that the tool is running, can be removed later
+	fmt.Println("tgen")
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Andrey Chernykh
+// SPDX-License-Identifier: MIT
+
+module github.com/andreychh/tgen
+
+go 1.25.5
