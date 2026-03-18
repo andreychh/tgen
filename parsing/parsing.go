d178e74b9d09ba30a5846731a57c817b087fc7ae
Andrey Chernykh <andreychernykh04@gmail.com>
2026-03-18 18:56:08 +0300

    refactor(#105): rename TypeTree to RawTree and introduce ExprTree and DefinitionDescription interfaces

--- parsing/parsing.go
+++ parsing/parsing.go
@@ -3,13 +3,37 @@
 
 package parsing
 
+import "iter"
+
+type DefinitionDescription interface {
+	Value() (string, error)
+}
+
+type ObjectName interface {
+	Value() (string, error)
+}
+
+// Variant represents a named type in a union definition.
+type Variant interface {
+	Name() ObjectName
+}
+
 type Optionality interface {
 	Value() (bool, error)
 }
 
+// Method represents a Telegram Bot API method definition.
+type Method interface {
+	Ref() DefinitionRef
+	Name() MethodName
+	Description() GQDefinitionDescription
+	Returns() TypeTree
+	Fields() iter.Seq[MethodField]
+}
+
 // FieldDescription represents the description column of a field table row.
 type FieldDescription interface {
-	RawValue
+	Value() (string, error)
 	// Links returns the hrefs of all anchor tags found in the description HTML.
 	Links() []string
 }
@@ -19,4 +43,8 @@ type Field interface {
 	Type() TypeTree
 	IsOptional() Optionality
 	Description() FieldDescription
+}
+
+type TypeTree interface {
+	Root() (TypeExpression, error)
 }
