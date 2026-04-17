ecad47118ef4e0b19dc155edb73185f7de9ed774
Andrey Chernykh <andreychernykh04@gmail.com>
2026-04-17 21:02:19 +0300

    fix(#150): handle bare InputFile KindObject in overlay

--- model/explicit/overlays/input_file.go
+++ model/explicit/overlays/input_file.go
@@ -23,11 +23,13 @@ func (o InputFile) Apply(field explicit.Field) explicit.Field {
 	if err != nil {
 		return field
 	}
-	if expr.Equals(types.NewUnion(
-		types.NewNamed("InputFile", types.KindObject),
-		types.NewNamed("String", types.KindPrimitive),
-	)) || expr.Equals(types.NewNamed("String", types.KindPrimitive)) &&
-		slices.Contains(links, "#sending-files") {
+	if expr.Equals(types.NewNamed("InputFile", types.KindObject)) ||
+		expr.Equals(types.NewUnion(
+			types.NewNamed("InputFile", types.KindObject),
+			types.NewNamed("String", types.KindPrimitive),
+		)) ||
+		(expr.Equals(types.NewNamed("String", types.KindPrimitive)) &&
+			slices.Contains(links, "#sending-files")) {
 		return NewModified(field, types.NewNamed("InputFile", types.KindUnion))
 	}
 	return field
