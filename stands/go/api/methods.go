66bc3b238b22b2acea138586bd2dc1fbdb4ee122
Andrey Chernykh <andreychernykh04@gmail.com>
2026-07-25 10:43:10 +0300

    refactor: update correct_option_id to correct_option_ids in API documentation

--- stands/go/api/methods.go
+++ stands/go/api/methods.go
@@ -763,7 +763,7 @@ func (m ForwardMessagesMethod) Call(ctx context.Context, conn Connection) ([]Mes
 // Use this method to copy messages of any kind. Service messages, paid media
 // messages, giveaway messages, giveaway winners messages, and invoice messages
 // can't be copied. A quiz poll can be copied only if the value of the field
-// correct_option_id is known to the bot. The method is analogous to the method
+// correct_option_ids is known to the bot. The method is analogous to the method
 // forwardMessage, but the copied message doesn't have a link to the original
 // message. Returns the MessageId of the sent message on success.
 //
@@ -912,7 +912,7 @@ func (m CopyMessageMethod) Call(ctx context.Context, conn Connection) (MessageID
 // messages can't be found or copied, they are skipped. Service messages, paid
 // media messages, giveaway messages, giveaway winners messages, and invoice
 // messages can't be copied. A quiz poll can be copied only if the value of the
-// field correct_option_id is known to the bot. The method is analogous to the
+// field correct_option_ids is known to the bot. The method is analogous to the
 // method forwardMessages, but the copied messages don't have a link to the
 // original message. Album grouping is kept for copied messages. On success, an
 // Array of MessageId of the sent messages is returned.
