4e0d37c6e1e477a04c18446258f9556e75251ae4
Andrey Chernykh <andreychernykh04@gmail.com>
2026-09-15 16:39:01 +0300

    refactor(#259): delete model/spec and the packages it fed

--- model/literals/literals.go
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Andrey Chernykh
-// SPDX-License-Identifier: MIT
-
-// Package literals provides statically-known implementations of model
-// interfaces. Currently contains Type for model.Type wrapping a known
-// TypeExpression.
-package literals
